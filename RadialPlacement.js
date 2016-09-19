a1d7918a2e1943de255dbdc7897de1e9e59fbc93
edcatley <edcat2@msn.com>
2016-09-19 19:24:30 +0100

    First commit

---
+++ RadialPlacement.js
@@ -0,0 +1,149 @@
+/*
+  Script for the automatic placement of components radially. Support for heirarchal designs and movement of Name and Designator to match footprint
+
+ */
+
+function bDrawClick(Sender){
+var sourceX,sourceY;
+var Source;
+var board = PCBServer.GetCurrentPCBBoard();
+var centerX = MMsToCoord(parseInt(Center_X_TEdit.Text));
+var centerY = MMsToCoord(parseInt(Center_Y_TEdit.Text));
+var startAngle = Start_Angle_TEdit.Text;
+var endAngle = End_Angle_TEdit.Text;
+var radius = MMsToCoord(Radius_TEdit.Text);
+var firstElementPrefix = First_Element_TEdit.Text.split(/[0-9]/);
+var firstElementSuffix = First_Element_TEdit.Text.split(/[A-Z]/);
+var lastElementPrefix = Last_Element_TEdit.Text.split(/[0-9]/);
+var lastElementSuffix =  Last_Element_TEdit.Text.split(/[A-Z]/);
+
+var firstRoomNamePrefix = First_Room_Name_TEdit.Text.split(/[0-9]/);
+var firstRoomSuffix = First_Room_Name_TEdit.Text.split(/[A-Z]/);
+var lastRoomNamePrefix = Last_Room_Name_TEdit.Text.split(/[0-9]/);
+var lastRoomSuffix =  Last_Room_Name_TEdit.Text.split(/[A-Z]/);
+
+var layer = String2Layer(layer_TEdit.Text);
+var angleOffset = parseFloat(Angle_Offset_TEdit.Text);
+var componentString;
+var angleStep = parseFloat(Angle_Step_TEdit.Text);
+var commentAngle, xCommentOffset;
+var nameAngle, xNameOffset;
+var angle = parseFloat(startAngle);
+var nameDistance, commentDistance;
+
+/*Automatic angle step calculation - 
+  if(firstRoomNamePrefix != "")
+    {
+    angleStep = (endAngle - startAngle)/(((parseInt(lastRoomSuffix)+1)-parseInt(firstRoomSuffix))*((parseInt(lastElementSuffix)+1)-parseInt(firstElementSuffix)));
+    }
+    else
+     {
+     angleStep = (endAngle - startAngle)/((parseInt(lastElementSuffix)+1)-parseInt(firstElementSuffix));
+     }
+     ShowMessage(angleStep);
+     */
+
+
+for (j = firstRoomSuffix; j <= lastRoomSuffix; j++)
+{
+for(i = firstElementSuffix; i<=lastElementSuffix; i++)
+    {
+
+    //Check to see if room names are used
+    if(firstRoomNamePrefix != "")
+    {
+    componentString = firstRoomNamePrefix+j+"_"+firstElementPrefix+i;
+    }
+    else
+    {
+    componentString =firstElementPrefix+i;
+    }
+    Source = board.GetPcbComponentByRefDes(componentString);
+    //Check to see if component exists
+    if(Source != null)
+        {
+
+    //required to prevent iterative name & comment drift
+      Source.Rotation=0;
+    Source.Comment.Rotation = 0;
+    Source.Name.Rotation= 0;
+    Source.AutoPosition_NameComment();
+
+    //Calculate Source XY
+    sourceX = radius*cos(angle* (Math.PI/180))+centerX;
+    sourceY = radius*sin(angle* (Math.PI/180))+centerY;
+
+    //Calculate x text offset
+    xNameOffset = Source.X  - Source.Name.XLocation;
+    xCommentOffset = Source.X  - Source.Comment.XLocation;
+
+    //Calculate text Y Distance
+    nameDistance = Math.sqrt(Math.pow(Source.Name.YLocation - Source.Y,2));
+    commentDistance = Math.sqrt(Math.pow(Source.Comment.YLocation - Source.Y,2));
+
+    //Calculate angle from origin for name and comment from arcos of the Y distance
+    commentAngle = Math.acos((Math.pow(commentDistance,2) - 2*(Math.pow(radius,2)))/(-2*radius*radius));
+    nameAngle = Math.acos((Math.pow(nameDistance,2) - 2*(Math.pow(radius,2)))/(-2*radius*radius));
+
+    //Calculate Name XY
+    NameX = (radius-xNameOffset)*cos(nameAngle+(angle*Math.PI/180))+centerX;
+    NameY =(radius-xNameOffset)*sin(nameAngle+(angle*Math.PI/180))+centerY;
+
+    //Calculate comment XY
+    CommentX = (radius-xCommentOffset)*cos(commentAngle+(angle*Math.PI/180))+centerX;
+    CommentY =(radius-xCommentOffset)*sin(commentAngle+(angle*Math.PI/180))+centerY;
+
+    //Move Source
+    Source.Rotation = angle+angleOffset;
+    Source.MoveToXY(sourceX,sourceY);
+     Source.Layer = layer;
+
+    //Move Name
+    Source.Name.MoveToXY(NameX,NameY);
+    Source.Name.Rotation = angle+angleOffset;
+
+    // Move comment
+    Source.Comment.MoveToXY(CommentX,CommentY);
+    Source.Comment.Rotation = angle+angleOffset;
+
+    // Increment angle
+    angle = angle + angleStep;
+    }
+
+    }
+    }
+Close();
+}
+
+
+
+ //..............................................................................
+
+//..............................................................................
+function bCloseClick(Sender)
+{
+    Close();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
